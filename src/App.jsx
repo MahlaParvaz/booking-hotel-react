8ea75c3db5ca9677976ffda188411da45dc7e9fa
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2023-11-24 10:20:06 +0330

    create hotel provider

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,23 @@ import Hotels from './components/Hotels/Hotels';
 import Footer from './components/Footer/Footer';
 import HotelsResult from './components/HotelsResult/HotelsResult';
 import HotelLayout from './components/HotelLayout/HotelLayout';
+import HotelResultProvider from './components/context/HotelResultProvider';
 function App() {
   return (
     <div className="bg-white ">
-      <Toaster />
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/hotels" element={<Hotels />} />
-        <Route path="/hotels-result" element={<HotelLayout />}>
-          <Route index element={<HotelsResult />} />
-          <Route path=":id" element={<div>single hotel</div>} />
-        </Route>
-      </Routes>
-      <Footer />
+      <HotelResultProvider>
+        <Toaster />
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/hotels" element={<Hotels />} />
+          <Route path="/hotels-result" element={<HotelLayout />}>
+            <Route index element={<HotelsResult />} />
+            <Route path=":id" element={<div>single hotel</div>} />
+          </Route>
+        </Routes>
+        <Footer />
+      </HotelResultProvider>
     </div>
   );
 }
--- src/components/HotelsResult/HotelsResult.jsx
+++ src/components/HotelsResult/HotelsResult.jsx
@@ -1,22 +1,16 @@
-import { Link, useSearchParams } from 'react-router-dom';
-import useFetch from '../../Hooks/useFetch';
+import { Link } from 'react-router-dom';
 import Loader from '../Loader/Loader';
+import { useHotels } from '../context/HotelResultProvider';
 
 function HotelsResult() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const destination = searchParams.get('destination');
-  const room = JSON.parse(searchParams.get('options'))?.room;
-  const { isLoading, data } = useFetch(
-    'http://localhost:5000/hotels',
-    `q=${destination || ''}&accommodates_gte=${room || 1}`
-  );
+  const { isLoading, hotels } = useHotels();
 
   if (isLoading) return <Loader />;
 
   return (
     <div className="searchList flex flex-col gap-8">
-      <h2>Search Results ({data.length})</h2>
-      {data.map((item) => {
+      <h2>Search Results ({hotels.length})</h2>
+      {hotels.map((item) => {
         return (
           <Link
             key={item.id}
