20a335ad8bb2a3187b7d403b37fb3561b0074f69
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2023-11-25 15:33:45 +0330

    delete bookmark ,use  state managment

--- src/components/AddNewBookmark/AddNewBookmark.jsx
+++ src/components/AddNewBookmark/AddNewBookmark.jsx
@@ -1,9 +1,10 @@
 import { useNavigate } from 'react-router-dom';
-import useUrlLocation from '../../hooks/useUrlLocation';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Loader from '../Loader/Loader';
 import ReactCountryFlag from 'react-country-flag';
+import { useBookmark } from '../context/BookmarkListProvider';
+import useUrlLocation from '../../Hooks/useUrlLocation';
 
 const BASE_GEOCODING_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
 
@@ -15,6 +16,7 @@ function AddNewBookmark() {
   const [countryCode, setCountryCode] = useState('');
   const [isLoadingGeoCoding, setIsLoadingGeoCoding] = useState(false);
   const [geoCodingError, setGeoCodingError] = useState(null);
+  const { createBookmark } = useBookmark();
 
   useEffect(() => {
     if (!lat || !lng) return;
@@ -39,13 +41,28 @@ function AddNewBookmark() {
     fetchLocationData();
   }, [lat, lng]);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!cityName || !country) return;
+
+    const newBookmark = {
+      cityName,
+      country,
+      countryCode,
+      latitude: lat,
+      longitude: lng,
+      host_location: cityName + ' ' + country,
+    };
+    await createBookmark(newBookmark);
+    navigate('/bookmark');
+  };
   if (isLoadingGeoCoding) return <Loader />;
   if (geoCodingError) return <storng>{geoCodingError}</storng>;
 
   return (
     <div>
       <h2 className="font-medium">Bookmark New Location</h2>
-      <form className="form mt-8">
+      <form className="form mt-8" onSubmit={handleSubmit}>
         <div className="formControl mb-4 relative">
           <label htmlFor="cityName " className="formLable block mb-2 ">
             CityName
--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -6,10 +6,16 @@ import Loader from '../Loader/Loader';
 import { useBookmark } from '../context/BookmarkListProvider';
 
 function Bookmark() {
-  const { isLoading, bookmarks } = useBookmark();
+  const { isLoading, bookmarks, currentBookmark, deleteBookmark } = useBookmark();
+
+  const handleDelete = async (e, id) => {
+    e.preventDefault();
+    await deleteBookmark(id);
+  };
 
   if (isLoading) return <Loader />;
-
+  if (!bookmarks.length) return <p>there is no bookmarked location</p>;
+ 
   return (
     <div>
       <h2 className=" my-4 font-medium">BookmarkList</h2>
@@ -21,13 +27,19 @@ function Bookmark() {
               key={item.id}
               to={`${item.id}?lat=${item.latitude}&lng=${item.longitude}`}
             >
-              <div className="bookmarkItem mb-4 border-solid border-[1px] border-slate-300 shadow hover:shadow-none rounded-2xl p-4 flex items-center justify-between ">
+              <div
+                className={`bookmarkItem mb-4 border-solid border-[1px] border-slate-300 shadow hover:shadow-none rounded-2xl p-4 flex items-center justify-between ${
+                  item.id === currentBookmark?.id
+                    ? 'current-bookmark border-solid border-[2px] -border--red -bg--light-gray '
+                    : ''
+                } `}
+              >
                 <div>
                   <ReactCountryFlag svg countryCode={item.countryCode} />
                   &nbsp; <strong>{item.cityName}</strong> &nbsp;
                   <span>{item.country}</span>
                 </div>
-                <button>
+                <button onClick={(e) => handleDelete(e, item.id)}>
                   <HiTrash className="trash -text--red w-5 h-5" />
                 </button>
               </div>
