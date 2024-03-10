db940f0c21f7be048c51ae56bbdf3f2c638de32b
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2024-03-10 14:47:25 +0330

    fix production bug

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { toast } from 'react-hot-toast';
+import toast from 'react-hot-toast';
 
 export default function useFetch(url, query = '') {
   const [data, setData] = useState([]);
