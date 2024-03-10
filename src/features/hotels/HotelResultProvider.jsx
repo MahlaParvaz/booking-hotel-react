134a7450c83b97417d98252876cd8cdd4c4b32e4
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2024-03-10 15:23:51 +0330

    fix production bug

--- src/features/hotels/HotelResultProvider.jsx
+++ src/features/hotels/HotelResultProvider.jsx
@@ -1,9 +1,8 @@
 import { createContext, useContext, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import toast from 'react-hot-toast';
-
-import http from '../../services/httpService';
 import useFetchData from '../../hooks/useFetchData';
+import toast from 'react-hot-toast';
+import http from '../../services/httpService';
 
 const HotelContext = createContext();
 const BASE_URL = 'http://localhost:5000/hotels';
