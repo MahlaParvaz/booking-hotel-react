6eb2f01dfafd8c3557e70421de5e523ed020c281
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2024-05-01 02:48:42 +0330

    change loader position

--- src/pages/HotelsResult.jsx
+++ src/pages/HotelsResult.jsx
@@ -6,23 +6,29 @@ import SearchResult from '../ui/SearchResult';
 function HotelsResult() {
   const { isLoading, currentHotel, hotels } = useHotels();
 
-  if (isLoading) {
-    return <Loader />;
-  }
+  // if (isLoading) {
+  //   return <Loader />;
+  // }
 
   return (
     <div className="flex z-50 justify-center items-center w-full mb-5">
       <div className="hotel-result-layout mb-130 ">
-        <div className="result__sidebar ">
-          <SearchResult
-            currentHotel={currentHotel}
-            isLoading={isLoading}
-            hotels={hotels}
-          />
-        </div>
-        <div className="result__map ">
-          <Map markerLocations={hotels} />
-        </div>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            <div className="result__sidebar ">
+              <SearchResult
+                currentHotel={currentHotel}
+                isLoading={isLoading}
+                hotels={hotels}
+              />
+            </div>
+            <div className="result__map ">
+              <Map markerLocations={hotels} />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- src/ui/SearchResult.jsx
+++ src/ui/SearchResult.jsx
@@ -2,42 +2,48 @@ import { Link } from 'react-router-dom';
 import Loader from './Loader';
 
 function SearchResult({ isLoading, currentHotel, hotels }) {
-  if (isLoading) return <Loader />;
+  // if (isLoading) return <Loader />;
 
   return (
     <div className="search-list flex flex-col gap-6 ">
       <h2 className="font-bold">Search Results ({hotels.length})</h2>
-      {hotels.map((item) => (
-        <Link
-          key={item.id}
-          to={`/hotels-result/${item.id}?lat=${item.latitude}&lng=${item.longitude}`}
-        >
-          <div
-            className={` search-list__item  ${
-              item.id === currentHotel?.id
-                ? 'current-hotel rounded-xl p-2  border-solid border-[2px] -border--dark-green'
-                : ''
-            }`}
-          >
-            <img
-              className="item__img "
-              src={item.picture_url.url}
-              alt={item.name}
-            />
-            <div className="item__desc mb-2  flex flex-col gap-2 mt-4">
-              <p className="location font-bold">{item.smart_location}</p>
-              <p className="name text-slate-500">{item.name}</p>
-              <p className="flex items-center font-semibold text-[15px] mobile:text-[14px]">
-                €&nbsp;{item.price}&nbsp;
-                <span className="text-slate-500">night</span>
-              </p>
-              <p className="btn btn--primary  px-1 py-1.5  rounded-full w-32 text-[13px] mt-3 ">
-                Book now
-              </p>
-            </div>
-          </div>
-        </Link>
-      ))}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {hotels.map((item) => (
+            <Link
+              key={item.id}
+              to={`/hotels-result/${item.id}?lat=${item.latitude}&lng=${item.longitude}`}
+            >
+              <div
+                className={` search-list__item  ${
+                  item.id === currentHotel?.id
+                    ? 'current-hotel rounded-xl p-2  border-solid border-[2px] -border--dark-green'
+                    : ''
+                }`}
+              >
+                <img
+                  className="item__img "
+                  src={item.picture_url.url}
+                  alt={item.name}
+                />
+                <div className="item__desc mb-2  flex flex-col gap-2 mt-4">
+                  <p className="location font-bold">{item.smart_location}</p>
+                  <p className="name text-slate-500">{item.name}</p>
+                  <p className="flex items-center font-semibold text-[15px] mobile:text-[14px]">
+                    €&nbsp;{item.price}&nbsp;
+                    <span className="text-slate-500">night</span>
+                  </p>
+                  <p className="btn btn--primary  px-1 py-1.5  rounded-full w-32 text-[13px] mt-3 ">
+                    Book now
+                  </p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </>
+      )}
     </div>
   );
 }
